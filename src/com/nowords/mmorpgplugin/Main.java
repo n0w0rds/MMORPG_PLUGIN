d04ddd602221f196b4e87aeed1f85a83fe3c1163
n0w0rds <76136069+n0w0rds@users.noreply.github.com>
2021-08-23 02:15:09 -0400

    Remove Textures things

--- src/com/nowords/mmorpgplugin/Main.java
+++ src/com/nowords/mmorpgplugin/Main.java
@@ -7,18 +7,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import com.nowords.mmorpgplugin.commands.ADMIN_COMMAND_MANAGER;
 import com.nowords.mmorpgplugin.entities.NPC;
-import com.nowords.mmorpgplugin.externalressources.TexturesAndModels;
-
 
 public class Main extends JavaPlugin{
 	
-	public TexturesAndModels textures;
 	public HashMap<String, NPC> NPC_LIST = new HashMap<>();
 	
 	@Override
 	public void onEnable() {
-		textures = new TexturesAndModels();
-		
 		getCommand("Admin").setExecutor(new ADMIN_COMMAND_MANAGER(this));
 	}
 }
